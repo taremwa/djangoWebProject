b56f2d527529ebcb14bd3a420118c60645ccff88
taremwa <marktaremwa92@gmail.com>
2024-07-29 17:03:11 +0300

    updating the database to postgres

--- expensesWebApp/settings.py
+++ expensesWebApp/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'expensesWebApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'IncomeExpensesdb',
+        'USER': 'postgres',
+        'PASSWORD': 'tareMWA',
+        'HOST': 'localhost',
     }
 }
 
