f54f0cd62e4057835f5d18c4c56176e7e406f7af
taremwa <marktaremwa92@gmail.com>
2024-08-09 11:47:01 +0300

    Api calls for user validation

--- authentication/views.py
+++ authentication/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 import json
 from django.http import JsonResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -12,8 +13,13 @@ class UsernameValidationView (View):
         username = data('username')
 
         if not str(username).isalnum():
-            return JsonResponse('username_error':'username should only contain alphanumeric characters')
+            return JsonResponse({'username_error':'username should only contain alphanumeric characters'}, status=400)
     
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error':'sorry! username already in use.'}, status=409)
+        return JsonResponse({'username_valid': True})
+
+
 class RegistrationView (View):
     def get(self, request):
-        return render(request, 'authentication/register.html')
+        return render(request, 'authentication/register.html') 
